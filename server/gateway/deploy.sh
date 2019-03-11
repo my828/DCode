36555f1069c6e0191f8e93dc7aa8a617729f3df6
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-03-10 23:12:09 -0700

    Updated gateway deploy script

--- server/gateway/deploy.sh
+++ server/gateway/deploy.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+deploy() {
+    bash ./build.sh
+
+    echo "Updating dcode-gateway image on DockerHub..."
+    docker push harshiakkaraju/dcode-gateway
+
+    # TODO -- ssh into ec2 instance
+    bash ./update.sh
+}
+
+deploy
