dec7a89da35f129a55ee6dac07356bf54e32ad68
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-03-10 23:14:19 -0700

    Added session handlers

--- server/gateway/main.go
+++ server/gateway/main.go
@@ -1,30 +1,58 @@
 package main
 
 import (
+	"DCode/server/gateway/handlers"
+	"DCode/server/gateway/sessions"
 	"log"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/go-redis/redis"
 
 	"github.com/gorilla/mux"
 )
 
+// Director is a starter http.Request that will be used in CustomDirector
+type Director func(r *http.Request)
+
+// CustomDirector modifies the request object before forwarding it to the microservice
+func CustomDirector() {
+	// @TODO: redirects to microservice
+}
+
+// HeartBeatHandler is a handler to check if the dcode server is alive
+func HeartBeatHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from DCode!"))
+}
+
 func main() {
+	signingKey := os.Getenv("SIGNINGKEY")
 	gatewayAddress := os.Getenv("GATEWAYADDRESS")
 	// tlsCertificate := os.Getenv("TLSCERT")
 	// tlsKey := os.Getenv("TLSKEY")
+	redisAddress := os.Getenv("REDISADDRESS")
 
-	// redisAddress := os.Getenv("REDISADDRESS")
+	redisDB := redis.NewClient(&redis.Options{
+		Addr: redisAddress,
+	})
+	redisStore := sessions.NewRedisStore(redisDB, time.Hour*48)
 
+	context := handlers.NewHandlerContext(signingKey, redisStore)
 	router := mux.NewRouter()
 
-	// @TODO: Replace with actual handlers
-	router.Handle("/v1/pages", nil)
-	router.Handle("v1/pages/{pageID}", nil)
-	router.Handle("v1/pages/{pageID}/canvas", nil)
-	router.Handle("v1/pages/{pageID}/editor", nil)
+	router.HandleFunc("/dcode", HeartBeatHandler)
+	router.HandleFunc("/dcode/v1/new", context.NewSessionHandler)
+	router.HandleFunc("/dcode/v1/{pageID}/extend", context.SessionExtensionHandler)
+	// @TODO: redirect to microservice
+	router.Handle("/dcode/v1/{pageID}", nil)
+	router.Handle("/dcode/v1/{pageID}/canvas", nil)
+	router.Handle("/dcode/v1/{pageID}/editor", nil)
 
 	// adds CORS middleware around handlers
-	// cors := handlers.NewCORSHandler(router)
+	cors := handlers.NewCORSHandler(router)
 
 	log.Printf("Server is listening on port: %s\n", gatewayAddress)
-	// log.Fatalf(http.ListenAndServeTLS(gatewayAddress, tlsCertificate, tlsKey, cors))
+	log.Fatal(http.ListenAndServe(gatewayAddress, cors))
+	// log.Fatal(http.ListenAndServeTLS(gatewayAddress, tlsCertificate, tlsKey, cors))
 }
