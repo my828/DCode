0ddf75eb858ab06df0a9c1a3476891fba3d8c1af
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-03-10 23:12:36 -0700

    Fixed a bug in CORS handler

--- server/gateway/handlers/cors.go
+++ server/gateway/handlers/cors.go
@@ -19,7 +19,7 @@ func NewCORSHandler(handlerToWrap http.Handler) *CORSHandler {
 	}
 }
 
-func (ch *CORSHandler) ServeHTTP(w http.ResponseWriter, r http.Request) {
+func (ch *CORSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Authorization")
@@ -30,5 +30,5 @@ func (ch *CORSHandler) ServeHTTP(w http.ResponseWriter, r http.Request) {
 		w.WriteHeader(http.StatusOK)
 		return
 	}
-	ch.ServeHTTP(w, r)
+	ch.handler.ServeHTTP(w, r)
 }
