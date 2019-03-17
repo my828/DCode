0385ff497e404d1da097f29c3d8cf9bc343c0928
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-03-16 17:15:40 -0700

    Websocket logic

--- client/src/Editor.js
+++ client/src/Editor.js
@@ -3,47 +3,46 @@
 import React from 'react';
 import Prism from 'prismjs';
 import Editor from 'react-simple-code-editor';
-// import { highlight, languages } from 'prismjs';
-// import './prism/prism.css'
+import { highlight, languages } from 'prismjs';
+import "./index.css";
 
 const code = "// Welcome to DCode! Copy and paste the link above to hare with friends. Type your code here:";
  
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   sessionID: this.props.state.sessionID,
-    //   code: this.props.state.code,
-    //   figures: this.props.state.figures
-    // }
+    this.state = {
+      pageState: this.props.state
+    }
   }
-  componentDidMount() {
-    this.setState({code: this.props.state.code})
-    console.log(this.state.code)
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      pageState: props.state
+    });
   }
-  // componentWillReceiveProps(props) {
-  //   this.setState({
-  //     sessionID: props.state.sessionID,
-  //     code: props.state.code,
-  //     figures: props.state.figures
-  //   });
-  // }
+
+  handleEditorChange(code) {
+    let state = this.state.pageState;
+    state.code = code;
+    this.props.updateState(state);
+  }
 
   state = { code }
   render() {
+    
     return (
         <Editor
-        value={this.state.code}
-        // onValueChange={code => this.updateCode(code)}
-        onValueChange={code => this.setState({ code }, () => {
-          this.props.update(this.state)
-        })}
-        highlight={code => Prism.highlight(code, Prism.languages.javascript, 'javascript')}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 15,
-        }}
+          className={"editor"}
+          value={this.state.pageState.code}
+          onValueChange={code => {this.handleEditorChange(code)}}
+          highlight={code => Prism.highlight(code, Prism.languages.javascript, 'javascript')}
+          padding={5}
+          style={{
+            fontFamily: 'Fira code, Fira Mono, monospace',
+            fontSize: '16px',
+            color: '#fff',
+          }}
       />
     );
   }
