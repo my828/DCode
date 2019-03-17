c54e894e0e3eb665b2272f804e0f8832fc5fd705
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-03-16 17:15:28 -0700

    Changed pen color + imported styles

--- client/src/Canvas.js
+++ client/src/Canvas.js
@@ -1,54 +1,53 @@
 import React from "react";
-import CanvasDraw from "react-canvas-draw";
-import Websocket from 'react-websocket';
 import {SketchField, Tools} from 'react-sketch';
 
 export default class Canvas extends React.Component {
-    constructor(prop) {
-        super(prop) 
+    constructor(props) {
+        super(props) 
         this.state = {
             width: window.screen.availWidth / 2 - window.screen.availWidth / 5,
+            pageState: props.state
         };
         this.canvasRef = React.createRef();
+        this.mounted = true;
     }
 
-    // componentWillReceiveProps(props) {
-    //     this.setState({
-    //         figures: props.figures
-    //     })
-    // }
+    componentWillReceiveProps(props) {
+        this.setState({
+            pageState: props.state
+        });
+    }
 
-    // componentDidUpdate() {
-    //     const socket = this.props.socket
-    //     socket.send(JSON.stringify(this.canvasRef.current.toJSON()))
-    // }
-
-    // componentDidMount() {
-    //     this.setState({
-    //         info: this.props.info
-    //     })     
-    // }
+    // handles changes to canvas
+    handleCanvasChange(evt) {
+        if (evt && (evt.type === "mouseup" || evt.type === "mouseout")) {
+            let figures = this.canvasRef.current.toJSON();
+            let pageState = this.state.pageState;
+            let path = window.location.href;
+            let components = path.split("/");
+            let sessionID = components[components.length - 1];
+            pageState.sessionID = sessionID;
+            pageState.figures = figures;
+            this.props.updateState(pageState);
+        }
+    }
 
     render() {
         return (
             <div>
-                <button type="button" class="btn btn-danger ml-2">
-                    Clear
+                <button type="button" className="btn btn-danger ml-2 canvas-clear">
+                    clear
                 </button>
-                <div class="d-flex">
+                <div className="d-flex">
                     <SketchField 
                             width='1024px'
                             ref={this.canvasRef}
-                            height='768px' 
+                            height='768px'
                             tool={Tools.Pencil} 
-                            lineColor='black'
+                            lineColor='blue'
                             lineWidth={3}
-                            // onChange={(e) => console.log(this.canvasRef.current.toJSON())}
-                            // this.props.sendCanvasData(this)
-                            onChange={(evt) => this.setState({figures: this.canvasRef.current.toJSON()},
-                             () => this.props.update(this.state))}
-                            // value={this.props.state.figures}
-                            defaultValue={this.props.state.figures}
+                            onChange={(evt) => {this.handleCanvasChange(evt)}}
+                            value={this.state.pageState.figures}
                     />
                 </div>
             </div>
