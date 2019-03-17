bcca0a83ffc59a7d624d2a56097e32b2376599fa
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-03-16 17:15:55 -0700

    Fixed styles

--- client/src/Main.js
+++ client/src/Main.js
@@ -2,60 +2,35 @@ import React from "react";
 import Editor from './Editor'
 import Canvas from './Canvas'
 import Nav from './Nav';
-// import { constants } from "http2";
+import { withRouter } from "react-router-dom";
+import './index.css';
 
-export default class Main extends React.Component {
-    constructor(props) {
-        super(props)
-        this.socket = null
+class Main extends React.Component {
+    componentDidMount() {
+        let socket = this.props.getSocket();
+        if (!socket) {
+            let path = window.location.href;
+            let components = path.split("/");
+            let sessionID = components[components.length - 1];
+            this.props.initializeSocket(sessionID);
+        }
     }
-    // componentDidMount() {
-    //     const API_WS = 'ws://localhost:4000/ws/'
-    //     const DCODE_API = "http://localhost:4000/dcode/";
-    //     fetch(`${DCODE_API}${this.props.state.sessionID}`, {
-    //         method: "GET",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         }
-    //     })
-    //     .then(res => {
-    //         return res.text()
-    //     })
-    //     .then((res) => {
-    //         const socket = new WebSocket(`${DCODE_API}${this.props.state.sessionID}`);
-    //         this.socket = socket
-    //         socket.onopen = () => {
-    //             console.log("CONNECT")
-    //         }
-    //         console.log(res)
-    //         this.props.socket(socket)
-    //         // this.setState({
-    //         //     sessionID: sessionID,
-                
-    //         // }, () => {
-    //         //     // check connection
-    //         //     socket.onopen = () => {
-    //         //         console.log("Socket Connect!");
-    //         //     }
-                
-    //         // });
-    //     })
-    //     .catch(err => {
-    //          window.alert("Session does not exits!")
-    //     })
-    // }
 
     render() {
         return(
             <div>
-                <Nav />
-                <div class="d-flex">
-                    {/* <Canvas socket={this.props.socket} info={this.props.info}/>
-                    <Editor socket={this.props.socket} info={this.props.info}/> */}
-                    <Canvas state={this.props.state} update={this.props.update}/>
-                    <Editor state={this.props.state} update={this.props.update}/>
+                <Nav/>
+                <div className={"main-container"}>
+                    <div className={"main-canvas-container"}>
+                        <Canvas state={this.props.state} updateState={this.props.updateState}/>
+                    </div>
+                    <div className={"main-editor-container"}>
+                        <Editor state={this.props.state} updateState={this.props.updateState} />
+                    </div>
                 </div>
             </div>
         )
     }
 }
+
+export default withRouter(Main);
