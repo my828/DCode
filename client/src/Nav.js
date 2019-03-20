d9b0fc161ce2078c581b7c9a10dabcbf05307b20
my828 <minyang828@gmail.com>
2019-03-19 23:23:27 -0700

    final change

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,18 +1,12 @@
 import React from "react";
-import logo from './logo.svg'
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './index.css';
 
 export default class Nav extends React.Component {
     render() {
-        let logostyle = {
-            height: "40px",
-            position: "relative",
-            left: "0"
-        }
         return (
             <nav className="navbar navbar-dark bg-dark">
-                <a href="/dcode"><span className={"lead text-white"}>DCode</span></a>
+                <Link to="/dcode"><span className={"lead text-white"}>DCode</span></Link>
                 <button className={"btn btn-sm btn-outline-warning"}>Extend Session</button>
             </nav>
         )
